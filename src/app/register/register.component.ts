45c5ae77929ece80f424a6c696c5595748bed7e3
simoncm <mollardsimon11@gmail.com>
2022-01-04 12:26:32 +0100

    starting to work on challenge-8

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { User } from './user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  public registerForm: FormGroup;
+
+  public user: User = new User();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+    });
+  }
+
+  public saveData(){
+    console.log(this.registerForm);
+    console.log('valeurs: ', JSON.stringify(this.registerForm.value));
+
+  }
+}
---
+++ src/app/register/user.ts
@@ -0,0 +1,7 @@
+export class User {
+
+  public firstName: string | undefined;
+  public lastName: string | undefined;
+  public email: string | undefined;
+
+}
